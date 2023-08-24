887532f9d2a7198e4aedf25d1fd9210e16f23e80
seonjuHa <gktjswn11@gmail.com>
2023-08-24 12:50:32 +0900

    #3 - ComponentScan 설정
    같은 이름의 클래스가 여러개 있어 컴파일 시 충돌 현상을 없애기 위해 컴포넌트 스캔을 사용하여 한 패키지만 독립적으로 컴파일 하는 방법을 채용 하여 이전에 했던 매퍼스캔 방법을 철회함

--- src/main/java/com/springsecurity/ssia_ch2_ex1/controller/HelloController.java
+++ src/main/java/com/springsecurity/ssia_ch2_ex1/controller/HelloController.java
@@ -4,8 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("ch2-ex1")
-@RequestMapping("/ch2-ex1")
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
--- src/main/java/com/springsecurity/ssia_ch2_ex2/controller/HelloController.java
+++ src/main/java/com/springsecurity/ssia_ch2_ex2/controller/HelloController.java
@@ -4,8 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("ch2-ex2")
-@RequestMapping("/ch2-ex2")
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
